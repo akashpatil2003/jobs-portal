5852fa556273f77c35a0f7a83f42683a76c59f5c
SkY <you@example.com>
2024-08-10 12:55:41 +0530

    Completed All remaining Fuctionalities

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import JobsPage from './pages/JobsPage'
 import AddJobPage from './pages/AddJobPage'
 import NotFounfPage from './pages/NotFounfPage'
 import SingleJobPage, {jobLoader} from './pages/SingleJobPage'
+import EditJobPage from './pages/EditJobPage'
 
 
 
@@ -27,6 +28,18 @@ const App = () => {
     });
     return;
   }
+  //Update job
+  const updateJob = async(job)=>{
+    const res = await fetch(`/api/jobs/${job.id}`,{
+      method: 'PUT',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  }
+
 
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -34,6 +47,7 @@ const App = () => {
       <Route index element={<HomePage />}/>
       <Route path='/jobs' element={<JobsPage />} />
       <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+      <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob}/>}  loader={jobLoader} />
       <Route path='/jobs/:id' element={<SingleJobPage deleteJob={ deleteJob}/>} loader={jobLoader} />
       <Route path='*' element={<NotFounfPage />} />
       
--- src/pages/SingleJobPage.jsx
+++ src/pages/SingleJobPage.jsx
@@ -1,5 +1,6 @@
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { useParams, useLoaderData, Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 function SingleJobPage({deleteJob}) {
   const navigate = useNavigate();
@@ -11,6 +12,7 @@ function SingleJobPage({deleteJob}) {
 
     if(!confirm) return;
     deleteJob(jobId);
+    toast.success('Job Deleted successfully');
     navigate('/jobs');
   }
   return (
@@ -84,7 +86,7 @@ function SingleJobPage({deleteJob}) {
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
               <Link
-                to="/add-job"
+                to={`/edit-job/${job.id}`}
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link>
               <button onClick={() => onDeleteClick(job.id)}
